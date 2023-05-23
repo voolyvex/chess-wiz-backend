a414e7174a8347db7aa3990ff50555f1e51d5cfc
voolyvex <1joeknapp@gmail.com>
2023-05-23 15:13:42 -0500

    fix: Removed unused views from url pattern

--- backend/pgn/urls.py
+++ backend/pgn/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('<int:pgn_pk>/', views.FetchPGNbyId.as_view()),
     path('assign/<str:student>/<int:pgn_pk>/', views.CoachAssignPGN.as_view()),
     path('favorites/', views.FetchFavorites.as_view()),
-    path('favorites/<int:pgn_pk>/', views.RemoveOrAddFavorite.as_view()),
     path('my_games/', views.FetchMyGames.as_view()),
     path('my_games/<int:pgn_pk>/', views.AddPgnToMyGames.as_view()),
     path('assigned/', views.FetchAssigned.as_view()),
